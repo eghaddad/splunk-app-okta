1cf635f8fff5658a369a84520bb26e9cdfb55b1a
eghaddad <ehaddad@ehaddad-mba13-1.local>
2015-07-29 19:58:59 -0700

    latest

--- splunk-app-okta/bin/ta_util/log_files.py
+++
@@ -1,9 +0,0 @@
-ta_util = "ta_util"
-ta_util_conf = "ta_util_conf"
-ta_util_rest = "ta_util_rest"
-ta_util_state_store = "ta_util_state_store"
-
-
-def get_all_logs():
-    g = globals()
-    return [g[log] for log in g if log.startswith("ta_")]
